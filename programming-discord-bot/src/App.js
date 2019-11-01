0ca88e6f94a8719b7871de68405a413ab1282160
Nick Bueltel <nick.bueltel@trintech.com>
2019-11-01 15:53:25 -0500

    link pages and navbar

--- programming-discord-bot/src/App.js
+++ programming-discord-bot/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from '../src/components/nav/navBar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <NavBar></NavBar>
     </div>
   );
 }
--- programming-discord-bot/src/components/nav/navBar.js
+++ programming-discord-bot/src/components/nav/navBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import UserPage from '../pages/user/userPage';
 
 class navBar extends React.Component {
     render() {
         return (
             <div>
                 <p> navBar works! </p>
+                <div>
+                    <UserPage></UserPage>
+                </div>
             </div>
         )
     };
